a10088c854166df50f7835e075ac44b3c158bd1e
Kaspars Avotins <kaspars.avotins@gmail.com>
2024-12-19 19:51:15 +0200

    added logger infra + some benchmarks

---
+++ TestDotNetAndStuff/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BenchmarkDotNet.Running;
+using TestDotNetAndStuff.Logger;
+
+Console.WriteLine("Hello, World!");
+
+// BenchmarkRunner.Run<LoggerBenchmarkTest>();
